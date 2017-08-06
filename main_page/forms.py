f1d7683848721bde3de356529c48542f94c16f8d
Broly941 <korzhawin@yahoo.com>
2017-08-06 20:07:18 +0300

    Modified style to Forms

--- main_page/forms.py
+++ main_page/forms.py
@@ -1,10 +1,30 @@
 from django import forms
 from .models import *
+from django.forms import TextInput
 
 
 class LinksForm(forms.ModelForm):
+    original = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder' : 'Введите ссылку...',
+        }
+    ))
+
+    description = forms.CharField(widget=forms.Textarea(
+        attrs={
+            'class' : 'form-control',
+            'placeholder' : 'Дайте описание...',
+        }
+    ))
 
     class Meta:
         model = Links
-        exclude = [""]
+        fields = (
+            'original',
+            'description',
+            'tag',
+        )
+        # exclude = [""]
+
 
--- main_page/views.py
+++ main_page/views.py
@@ -1,8 +1,10 @@
 import operator
-
 from django.shortcuts import render, redirect, get_object_or_404
+from django.utils import timezone
 from .models import Links, Tags
 from .forms import LinksForm
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 
 
 def main_page(request):
@@ -12,6 +14,20 @@ def main_page(request):
 
 
 def create_link(request):
+    tags = Tags.objects.all()
+    ordered_tags = sorted(tags, key=operator.attrgetter('name'))
+
+    form = LinksForm(request.POST)
+    if form.is_valid():
+        link = form.save(commit=False)
+
+        link.author = request.user
+        link.shortcut = 'Here should be short link.'
+        link.created_date = timezone.now()
+        link.save()
+        return redirect('links_detail', pk=link.pk)
+    else:
+        form = LinksForm()
     return render(request, 'main_page/create_link.html', locals())
 
 
@@ -26,6 +42,7 @@ def tags_detail(request, pk):
     tags = get_object_or_404(Tags, pk=pk)
     links = Links.objects.all()
     return render(request, 'main_page/tags_detail.html', locals())
+
 
 def links_detail(request, pk):
     links = get_object_or_404(Links, pk=pk)
