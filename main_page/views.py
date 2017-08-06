b3de97452f816c710ecad479bd7ec8aa9d9e7667
Broly941 <korzhawin@yahoo.com>
2017-08-07 01:11:29 +0300

    Ready link shortcut

--- main_page/views.py
+++ main_page/views.py
@@ -3,9 +3,11 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from .models import Links, Tags
 from .forms import LinksForm
-from django.contrib import messages
-from django.http import HttpResponse, HttpResponseRedirect, Http404
 
+import requests
+import json
+
+GOOGLE_URL_SHORTEN_API = 'AIzaSyCeZMN-uyaLrbEACHreeLJLFzGBi2LvoJY'
 
 def main_page(request):
     links = Links.objects.all()
@@ -22,7 +24,7 @@ def create_link(request):
         link = form.save(commit=False)
 
         link.author = request.user
-        link.shortcut = 'Here should be short link.'
+        link.shortcut = google_url_shorten(form.cleaned_data['original'])
         link.created_date = timezone.now()
         link.save()
         return redirect('links_detail', pk=link.pk)
@@ -47,3 +49,12 @@ def tags_detail(request, pk):
 def links_detail(request, pk):
     links = get_object_or_404(Links, pk=pk)
     return render(request, 'main_page/links_detail.html', locals())
+
+def google_url_shorten(url):
+   req_url = 'https://www.googleapis.com/urlshortener/v1/url?key=' + GOOGLE_URL_SHORTEN_API
+   payload = {'longUrl': url}
+   headers = {'content-type': 'application/json'}
+   r = requests.post(req_url, data=json.dumps(payload), headers=headers)
+   resp = json.loads(r.text)
+   return resp['id']
+
